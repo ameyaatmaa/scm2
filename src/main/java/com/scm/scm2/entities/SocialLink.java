05c393b3cc1894b3deb121257a0aad68227ead1e
ameyaatmaa <absharmad1818@gmail.com>
2025-08-03 22:45:49 +0530

    Created Entitites like user contact

---
+++ src/main/java/com/scm/scm2/entities/SocialLink.java
@@ -0,0 +1,35 @@
+package com.scm.scm2.entities;
+
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class SocialLink {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String link;
+    private String title;
+
+    @ManyToOne
+    private Contact contact;
+
+
+    @OneToMany(mappedBy = "contact", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    private List<SocialLink> links = new ArrayList<>();
+}
---
+++ src/main/java/com/scm/scm2/entities/User.java
@@ -0,0 +1,49 @@
+package com.scm.scm2.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name="user")
+@Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class User {
+
+    @Id
+    private String userId;
+
+    @Column(name="user_name",nullable=false)
+    private String name;
+    @Column(name="user_name",nullable=false,unique=true)
+    private String email;
+    private String password;
+
+    @Column(length = 10000)
+    private String about;
+
+    @Column(length = 10000)
+    private String profilePic;
+    private String phoneNumber;
+
+    private boolean enabled=false;
+    private boolean emailVerified=false;
+    private boolean phoneVerified=false;
+
+    // self google ,fb ,github etc how its signing up
+
+    private Providers provider=Providers.SELF;
+    private String providerUserId;
+
+    @OneToMany(mappedBy = "user",cascade=CascadeType.ALL,fetch =  FetchType.LAZY,orphanRemoval = true)
+    private List<Contact> contacts= new ArrayList<>();
+
+
+}
