c58f5f4c1b73e2b12f47989778209d7ca432699c
ameyaatmaa <absharmad1818@gmail.com>
2025-08-07 23:36:26 +0530

    .

---
+++ src/main/java/com/scm/scm2/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.scm.scm2.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+//    user create and login using java code
+
+
+
+}
--- src/main/java/com/scm/scm2/forms/UserForm.java
+++ src/main/java/com/scm/scm2/forms/UserForm.java
@@ -1,6 +1,9 @@
 package com.scm.scm2.forms;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -11,10 +14,23 @@ import lombok.*;
 @ToString
 public class UserForm {
 
+    @NotBlank(message = "Username is required")
+    @Size(min=3,message = "Min 3 Chracters in required")
     private String name;
+
+    @Email(message = "Invalid Email")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min=6,message = "Min 6 Characters is required")
     private String password;
+
+    @NotBlank(message = "About is required")
+    @Size(max = 250,message = "Only upto 250 Characters is allowed")
     private String about;
+
+    @Size(min = 8, max = 12, message = "Invalid Phone Number")
     private String phoneNumber;
 
 
